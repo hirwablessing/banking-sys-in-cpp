6d300f8ec1d69bd276ca2c9c5c9280edb4daeeda
Blessing Hirwa <hirwablessing7@gmail.com>
2022-07-04 21:55:19 +0200

    feat: add logging to the system

--- banking-system.cpp
+++ banking-system.cpp
@@ -1,3 +1,6 @@
+// running the program
+// g++ banking-system.cpp -o bank
+
 #include <iostream>
 #include <iomanip>
 #include <time.h>
@@ -6,6 +9,29 @@
 #include <vector>
 
 using namespace std;
+
+string search_account(long account_number)
+{
+	ifstream file;
+	file.open("accounts.txt");
+	string line;
+	string result = "";
+	while (getline(file, line))
+	{
+		if (line.find(to_string(account_number)) != string::npos)
+		{
+			result = line;
+		}
+	}
+	file.close();
+
+	if (result == "")
+	{
+		cout << "Account not found";
+	}
+
+	return result;
+}
 
 class Account
 {
@@ -33,7 +59,14 @@ public:
 
 	Account()
 	{
+	create_account:
 		acc_no = generateAccountNumber();
+
+		string doesAccountExist = search_account(acc_no);
+
+		if (doesAccountExist != "")
+			goto create_account;
+
 		issue_date = generateIssueDate();
 	}
 };
@@ -102,11 +135,14 @@ public:
 	}
 };
 
-void create_account(AccountList *acc_list)
+void createAccount(AccountList *acc_list)
 {
 	int nberOfAccounts;
 	ofstream file;
+	ofstream logs;
+
 	file.open("accounts.txt", ios::app);
+	logs.open("logs.txt", ios::app);
 
 	cout << "Enter number of accounts to create: ";
 	cin >> nberOfAccounts;
@@ -125,44 +161,23 @@ void create_account(AccountList *acc_list)
 		cout << "Account number for " << acc->names << " is: " << acc->acc_no << endl;
 		acc_list->enqueue(acc);
 		file << acc->acc_no << "\t" << acc->names << "\t" << acc->dob << "\t" << acc->issue_date << "\t" << acc->balance << endl;
+		logs << acc->acc_no << "\t" << acc->names << "\t" << acc->dob << "\t" << acc->issue_date << "\t" << acc->balance << endl;
 	}
 	file.close();
+	logs.close();
 }
 
-void get_all_accounts(AccountList *acc_list)
+void get_all_accounts()
 {
 	cout << "Account Number - Holder Names - Date Of Birth - Date Of Issue - Balance " << endl;
 	ifstream file;
-	file.open("accounts.txt");
+	file.open("accounts.csv");
 	string line;
 	while (getline(file, line))
 	{
 		cout << line << endl;
 	}
 	file.close();
-}
-
-string search_account(long account_number)
-{
-	ifstream file;
-	file.open("accounts.txt");
-	string line;
-	string result = "";
-	while (getline(file, line))
-	{
-		if (line.find(to_string(account_number)) != string::npos)
-		{
-			result = line;
-		}
-	}
-	file.close();
-
-	if (result == "")
-	{
-		cout << "Account not found";
-	}
-
-	return result;
 }
 
 void tokenizestring(string const &str, const char delim, vector<string> &out)
@@ -176,22 +191,29 @@ void tokenizestring(string const &str, const char delim, vector<string> &out)
 	}
 }
 
+// a function that replaces the current line in the file with the new line
+
 void update_balance(string action)
 {
 	ifstream file;
 	ofstream newfile;
+	ofstream logs;
+
 	file.open("accounts.txt");
 	newfile.open("newaccounts.txt");
+	logs.open("logs.txt");
+
 	string line;
 
 	long account_number;
 	float amount;
 	cout << "Enter account number: ";
 	cin >> account_number;
-	cout << "Enter amount: ";
-	cin >> amount;
 
 	string found_account = search_account(account_number);
+
+	cout << "Enter amount: ";
+	cin >> amount;
 
 	if (found_account != "")
 	{
@@ -224,14 +246,17 @@ void update_balance(string action)
 				cout << "Balance after transaction: " << balance << endl;
 
 				newfile << words[0] << "\t" << words[1] << "\t" << words[2] << "\t" << words[3] << "\t" << balance << endl;
+				logs << words[0] << "\t" << words[1] << "\t" << words[2] << "\t" << words[3] << "\t" << balance << endl;
 			}
 			else
 			{
 				newfile << line << endl;
 			}
 		}
+
 		file.close();
 		newfile.close();
+		logs.close();
 		remove("accounts.txt");
 		rename("newaccounts.txt", "accounts.txt");
 	}
@@ -241,8 +266,10 @@ void update_account_details()
 {
 	ifstream file;
 	ofstream newfile;
+	ofstream bankLogs;
 
 	file.open("accounts.txt");
+	file.open("logs.txt");
 	newfile.open("newaccounts.txt");
 
 	string line, new_names, new_dob;
@@ -263,11 +290,13 @@ void update_account_details()
 		cin >> new_dob;
 		while (getline(file, line))
 		{
+
 			if (found_account == line)
 			{
 				vector<string> words;
 				tokenizestring(line, '\t', words);
 				newfile << words[0] << "\t" << new_names << "\t" << new_dob << "\t" << words[3] << "\t" << words[4] << endl;
+				bankLogs << words[0] << "\t" << new_names << "\t" << new_dob << "\t" << words[3] << "\t" << words[4] << endl;
 			}
 			else
 			{
@@ -305,77 +334,110 @@ void close_account()
 				newfile << line << endl;
 			}
 		}
-		cout<<endl;
+		cout << endl;
 		file.close();
 		newfile.close();
 		remove("accounts.txt");
 		rename("newaccounts.txt", "accounts.txt");
-		cout << "Account closed successfully" << endl<<endl;
-		
+		cout << "Account closed successfully" << endl
+			 << endl;
 	}
-
 }
 
-		void menu(AccountList * account_list)
+void viewAccountLogs()
+{
+	long account_number;
+
+	cout << "Enter account number: ";
+	cin >> account_number;
+
+	ifstream file;
+	file.open("accounts.txt");
+	string line;
+	bool hastRecord = false;
+
+	while (getline(file, line))
+	{
+		if (line.find(to_string(account_number)) != string::npos)
 		{
-			int option;
-			long account_number;
-
-			do
-			{
-				cout << "\t===== WELCOME TO BANK MANAGEMENT SYSTEMS =====  |" << endl;
-				cout << "\t|\t\t1. CREATE ACCOUNT\t\t|" << endl;
-				cout << "\t|\t\t2. BANK ENQUIRY\t\t\t|" << endl;
-				cout << "\t|\t\t3. DEPOSIT AMOUNT\t\t|" << endl;
-				cout << "\t|\t\t4. WITHDRAW AMOUNT\t\t|" << endl;
-				cout << "\t|\t\t5. CLOSE ACCOUNT\t\t|" << endl;
-				cout << "\t|\t\t6. MODIFY ACCOUNT\t\t|" << endl;
-				cout << "\t|\t\t7. ACCOUNTS LIST\t\t|" << endl;
-				cout << "\t|\t\t8. EXIT\t\t\t\t|" << endl
-					 << endl;
-
-				cout << "\t\tOption (1-8): ";
-				cin >> option;
-
-				system("cls");
-				switch (option)
-				{
-				case 1:
-					create_account(account_list);
-					break;
-				case 2:
-					cout << "Enter account number: ";
-					cin >> account_number;
-					cout << search_account(account_number) << endl;
-					break;
-				case 3:
-					update_balance("deposit");
-					break;
-				case 4:
-					update_balance("withdraw");
-					break;
-				case 5:
-					close_account();
-					break;
-				case 6:
-					update_account_details();
-					break;
-				case 7:
-					get_all_accounts(account_list);
-					break;
-				case 8:
-					cout << endl
-						 << "\t\tThanks For Using Our Services!" << endl;
-					break;
-				default:
-					cout << endl
-						 << "\t\tUnknown Option! Please Choose from 1 to 8" << endl;
-				}
-			} while (option != 8);
+			cout << line << endl;
+			hastRecord = true;
 		}
+	}
+	file.close();
 
-		int main()
+	if (hastRecord == false)
+	{
+		cout << "\n\n";
+		cout << "No records found" << endl;
+		cout << "\n\n";
+	}
+}
+
+void menu(AccountList *account_list)
+{
+	int option;
+	long account_number;
+
+	do
+	{
+		cout << "\t===== WELCOME TO BANK MANAGEMENT SYSTEMS =====  |" << endl;
+		cout << "\t|\t\t1. CREATE ACCOUNT\t\t|" << endl;
+		cout << "\t|\t\t2. BANK ENQUIRY\t\t\t|" << endl;
+		cout << "\t|\t\t3. DEPOSIT AMOUNT\t\t|" << endl;
+		cout << "\t|\t\t4. WITHDRAW AMOUNT\t\t|" << endl;
+		cout << "\t|\t\t5. CLOSE ACCOUNT\t\t|" << endl;
+		cout << "\t|\t\t6. MODIFY ACCOUNT\t\t|" << endl;
+		cout << "\t|\t\t7. ACCOUNTS LIST\t\t|" << endl;
+		cout << "\t|\t\t8. ACCOUNT LOGS\t\t\t|" << endl;
+		cout << "\t|\t\t9. EXIT\t\t\t\t|" << endl
+			 << endl;
+
+		cout << "\t\tOption (1-8): ";
+		cin >> option;
+
+		system("clear");
+		switch (option)
 		{
-			AccountList *account_list = new AccountList();
-			menu(account_list);
+		case 1:
+			createAccount(account_list);
+			break;
+		case 2:
+			cout << "Enter account number: ";
+			cin >> account_number;
+			cout << search_account(account_number) << endl;
+			break;
+		case 3:
+			update_balance("deposit");
+			break;
+		case 4:
+			update_balance("withdraw");
+			break;
+		case 5:
+			close_account();
+			break;
+		case 6:
+			update_account_details();
+			break;
+		case 7:
+			get_all_accounts();
+			break;
+		case 8:
+			viewAccountLogs();
+			break;
+		case 9:
+			cout << endl
+				 << "\t\tThanks For Using Our Services!" << endl;
+			break;
+		default:
+			cout << endl
+				 << "\t\tUnknown Option! Please Choose from 1 to 8" << endl;
 		}
+	} while (option != 9);
+}
+
+int main()
+{
+	AccountList *account_list = new AccountList();
+	menu(account_list);
+}
